a00c9569192a44f3da56419d277ae5acdda35283
Jang184 <e2joo418@gmail.com>
2022-04-20 17:40:58 +0900

    delete git cash

--- .build/src/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUser = exports.getUser = void 0;
-const User_1 = require("./User");
-Object.defineProperty(exports, "getUser", { enumerable: true, get: function () { return User_1.getUser; } });
-Object.defineProperty(exports, "createUser", { enumerable: true, get: function () { return User_1.createUser; } });
--- .build/src/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserDao = void 0;
-const UserDao_1 = __importDefault(require("./UserDao"));
-exports.UserDao = UserDao_1.default;
